dee42c42ada4e34398d3f4b4cdbc727b0bae21d9
Eftekhar Nishan <is061270@gmail.com>
2026-06-14 14:45:26 +0600

    practiced basic maths operations

---
+++ BasicMaths.java
@@ -0,0 +1,17 @@
+// 1. Declare two integer variables, 
+// 2. Calculate Addition, Subtraction, Multiplication, Division, 
+// and 3. Display all results.
+
+public class BasicMaths {
+
+    public static void main(String[] args) {
+
+        int a = 20; 
+        int b = 5;
+
+        System.out.println("Addition: " + (a + b));        
+        System.out.println("Subtraction: " + (a - b));     
+        System.out.println("Multiplication: " + (a * b));  
+        System.out.println("Division: " + (a / b));        
+    }
+}
